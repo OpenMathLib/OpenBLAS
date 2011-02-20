0cfd29a819a7182111b6a4d63a653d011db561de
Xianyi Zhang <xianyi@iscas.ac.cn>
2011-02-21 00:24:21 +0800

    Fixed #7. 1)Disable the multi-thread and  2) Modified kernel codes to avoid unloop in axpy function when incx==0 or incy==0.

--- interface/zaxpy.c
+++ interface/zaxpy.c
@@ -83,6 +83,11 @@ void CNAME(blasint n, FLOAT *ALPHA, FLOAT *x, blasint incx, FLOAT *y, blasint in
 #ifdef SMP
   nthreads = num_cpu_avail(1);
 
+  //disable multi-thread when incx==0 or incy==0
+  //In that case, the threads would be dependent.
+  if (incx == 0 || incy == 0)
+	  nthreads = 1;
+
   if (nthreads == 1) {
 #endif
 
