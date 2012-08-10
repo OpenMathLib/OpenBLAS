d007cca61d415af7c16dc51f2ce1c013861b24a4
Zhang Xianyi <xianyi@iscas.ac.cn>
2012-08-10 11:54:21 +0800

    Refs #134. Fixed the building bug on IBM Power.

--- common_interface.h
+++ common_interface.h
@@ -45,7 +45,7 @@ extern "C" {
 
 int    BLASFUNC(xerbla)(char *, blasint *info, blasint);
 
-void    BLASFUNC(openblas_set_num_threads)(int *);
+void    openblas_set_num_threads_(int *);
 
 FLOATRET  BLASFUNC(sdot)  (blasint *, float  *, blasint *, float  *, blasint *);
 FLOATRET  BLASFUNC(sdsdot)(blasint *, float  *,        float  *, blasint *, float  *, blasint *);
