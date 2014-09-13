5194818d4b1a2dc7d5a4768eadecb0f6c29432ec
wernsaar <wernsaar@googlemail.com>
2014-09-13 09:48:34 +0200

    updated zgemv_t_4.c

--- kernel/x86_64/zgemv_t_4.c
+++ kernel/x86_64/zgemv_t_4.c
@@ -32,7 +32,7 @@ USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #if defined(BULLDOZER) || defined(PILEDRIVER)
 #include "zgemv_t_microk_bulldozer-4.c"
 #elif defined(HASWELL)
-#include "zgemv_t_microk_haswell-2.c"
+#include "zgemv_t_microk_haswell-4.c"
 #endif
 
 
