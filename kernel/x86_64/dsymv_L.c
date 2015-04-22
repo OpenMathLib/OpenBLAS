1bec9abb9ad581aab170645451e2ad45fc955dd9
Werner Saar <wernsaar@googlemail.com>
2015-04-22 12:09:43 +0200

    added optimized dsymv kernels for sandybridge

--- kernel/x86_64/dsymv_L.c
+++ kernel/x86_64/dsymv_L.c
@@ -32,6 +32,8 @@ USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "dsymv_L_microk_bulldozer-2.c"
 #elif defined(HASWELL)
 #include "dsymv_L_microk_haswell-2.c"
+#elif defined(SANDYBRIDGE)
+#include "dsymv_L_microk_sandy-2.c"
 #elif defined(NEHALEM)
 #include "dsymv_L_microk_nehalem-2.c"
 #endif
