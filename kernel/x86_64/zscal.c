7de6bb98892129b36e5b911efeaa53472ba67c80
Werner Saar <wernsaar@googlemail.com>
2015-05-17 11:45:19 +0200

    added optimized zscal kernel for bulldozer

--- kernel/x86_64/zscal.c
+++ kernel/x86_64/zscal.c
@@ -30,6 +30,8 @@ USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #if defined(HASWELL)
 #include "zscal_microk_haswell-2.c"
+#elif defined(BULLDOZER)
+#include "zscal_microk_bulldozer-2.c"
 #endif
 
 
