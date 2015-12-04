958f0de65e234e03b274871b018407807a786326
Zhang Xianyi <traits.zhang@gmail.com>
2015-12-05 00:45:29 +0800

    Refs #708. Modified config template for MSVC.

--- openblas_config_template.h
+++ openblas_config_template.h
@@ -58,9 +58,8 @@ typedef int blasint;
 /* C99 supports complex floating numbers natively, which GCC also offers as an
    extension since version 3.0.  If neither are available, use a compatible
    structure as fallback (see Clause 6.2.5.13 of the C99 standard). */
-#if (defined(__STDC_IEC_559_COMPLEX__) || __STDC_VERSION__ >= 199901L || \
-     (__GNUC__ >= 3 && !defined(__cplusplus)) || \
-     (_MSC_VER >= 1800 && !defined(__cplusplus))) // Visual Studio 2013 supports complex
+#if ((defined(__STDC_IEC_559_COMPLEX__) || __STDC_VERSION__ >= 199901L || \
+      (__GNUC__ >= 3 && !defined(__cplusplus))) && !(defined(FORCE_OPENBLAS_COMPLEX_STRUCT)))
   #define OPENBLAS_COMPLEX_C99
 #ifndef __cplusplus
   #include <complex.h>
