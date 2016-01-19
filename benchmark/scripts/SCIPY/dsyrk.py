a48b247e9e17120f3efe83baa43823c9a3f25fe1
John Kirkham <kirkhamj@janelia.hhmi.org>
2016-01-19 15:32:28 -0500

    benchmark/scripts/SCIPY/dsyrk.py: Overwrite will work on a Fortran array of the correct type.

--- benchmark/scripts/SCIPY/dsyrk.py
+++ benchmark/scripts/SCIPY/dsyrk.py
@@ -16,7 +16,7 @@ def run_dsyrk(N, l):
 
     start = time.time()
     for i in range(0, l):
-        C[...] = blas.dsyrk(1.0, A)
+        blas.dsyrk(1.0, A, c=C, overwrite_c=True)
     end = time.time()
 
     timediff = (end - start)
