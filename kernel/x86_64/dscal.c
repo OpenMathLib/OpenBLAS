c44ff4d648c38a0f206b37c17c7a7dbfa8f30e50
Zhang Xianyi <traits.zhang@gmail.com>
2016-01-28 04:38:07 +0800

    Refs #714. avoid compiling warnings.

--- kernel/x86_64/dscal.c
+++ kernel/x86_64/dscal.c
@@ -87,7 +87,7 @@ static void dscal_kernel_inc_8(BLASLONG n, FLOAT *alpha, FLOAT *x, BLASLONG inc_
 static void dscal_kernel_inc_8(BLASLONG n, FLOAT *alpha, FLOAT *x, BLASLONG inc_x)
 {
 
-	FLOAT *x1;
+	FLOAT *x1=NULL;
 	BLASLONG inc_x3;
 
 	inc_x <<= 3;
