a94f2b78482aea0fe826e61349bd857e3797f89f
Daniel Patrick Foose <dpfoose@gmail.com>
2016-06-14 14:37:28 -0400

    Change to allow compiling with USE_OPENMP on MSVC
    MSVC treats the declaration of omp_in_parallel and omp_get_num_procs without the modifiers __declspec(dllimport) and __cdecl as a redefinition.

--- common.h
+++ common.h
@@ -626,8 +626,13 @@ void gotoblas_profile_init(void);
 void gotoblas_profile_quit(void);
 
 #ifdef USE_OPENMP
+#ifndef C_MSVC
 int omp_in_parallel(void);
 int omp_get_num_procs(void);
+#else
+__declspec(dllimport) int __cdecl omp_in_parallel(void);
+__declspec(dllimport) int __cdecl omp_get_num_procs(void);
+#endif
 #else
 #ifdef __ELF__
 int omp_in_parallel  (void) __attribute__ ((weak));
