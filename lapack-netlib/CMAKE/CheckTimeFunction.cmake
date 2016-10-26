fb7057babe451999a5111b450a7989622fcabe6c
Martin Köhler <koehlerm@mpi-magdeburg.mpg.de>
2016-10-26 15:03:16 +0200

    Import LAPACK: CMAKE directory

--- lapack-netlib/CMAKE/CheckTimeFunction.cmake
+++ lapack-netlib/CMAKE/CheckTimeFunction.cmake
@@ -16,11 +16,11 @@ macro(CHECK_TIME_FUNCTION FUNCTION VARIABLE)
     if(RES)
       set(${VARIABLE} ${FUNCTION} CACHE INTERNAL "Have Fortran function ${FUNCTION}")
       message(STATUS "Looking for Fortran ${FUNCTION} - found")
-      file(APPEND ${PROJECT_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeOutput.log
+      file(APPEND ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeOutput.log
         "Fortran ${FUNCTION} exists. ${OUTPUT} \n\n")
     else(RES)
       message(STATUS "Looking for Fortran ${FUNCTION} - not found")
-      file(APPEND ${PROJECT_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeError.log
+      file(APPEND ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeError.log
         "Fortran ${FUNCTION} does not exist. \n ${OUTPUT} \n")
     endif(RES)
 endmacro(CHECK_TIME_FUNCTION)
