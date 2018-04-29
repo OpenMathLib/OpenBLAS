8145ecd70bdfae44f62b5ff9a9e0ee427a2db3db
Martin Kroeker <martin@ruby.chemie.uni-freiburg.de>
2018-04-29 14:38:55 +0200

    Avoid out-of-bounds reads from blas_quick_divide_table on big systems

--- common_x86.h
+++ common_x86.h
@@ -179,6 +179,10 @@ static __inline int blas_quickdivide(unsigned int x, unsigned int y){
   return result;
 #else
 
+  if ( y > 64) {
+	  result = x/y;
+	  return result;
+  }	  
   y = blas_quick_divide_table[y];
 
   __asm__ __volatile__  ("mull %0" :"=d" (result) :"a"(x), "0" (y));
