cacacc8007eaf8c01ca32f289980ee8b91016b8f
Arjan van de Ven <arjan@linux.intel.com>
2018-08-11 17:14:57 +0000

    Add an AVX512 enabled DSCAL function
    written in C intrinsics for best readability.
    (the same C code works for Haswell as well)
    
    For logistical reasons the code falls back to the existing
    haswell AVX2 implementation if the GCC or LLVM compiler is not new enough

--- kernel/x86_64/dscal.c
+++ kernel/x86_64/dscal.c
@@ -31,8 +31,10 @@ USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "dscal_microk_bulldozer-2.c"
 #elif defined(SANDYBRIDGE)
 #include "dscal_microk_sandy-2.c"
-#elif defined(HASWELL) || defined(ZEN) || defined (SKYLAKEX)
+#elif defined(HASWELL) || defined(ZEN)
 #include "dscal_microk_haswell-2.c"
+#elif  defined (SKYLAKEX)
+#include "dscal_microk_skylakex-2.c"
 #endif
 
 
