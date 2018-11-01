9c177d270b7ae78c4542a15ec02d8cab9cc7f367
Martin Kroeker <martin@ruby.chemie.uni-freiburg.de>
2018-11-01 18:50:25 +0100

    Restore Android/ARMv7 build fix from #778
    for #1811

--- lapack-netlib/LAPACKE/include/lapacke_config.h
+++ lapack-netlib/LAPACKE/include/lapacke_config.h
@@ -34,6 +34,13 @@
 #ifndef _LAPACKE_CONFIG_H_
 #define _LAPACKE_CONFIG_H_
 
+// For Android prior to API 21 (no <complex> include)
+#if defined(__ANDROID__)
+#if __ANDROID_API__ < 21
+#define LAPACK_COMPLEX_STRUCTURE
+#endif
+#endif
+
 #ifdef __cplusplus
 #if defined(LAPACK_COMPLEX_CPP)
 #include <complex>
