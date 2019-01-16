e8d835ea466a1605db2157b6884a4cfe762478fc
Martin Kroeker <martin@ruby.chemie.uni-freiburg.de>
2019-01-16 21:09:03 +0100

    Tag operands 0 and 1 as both input and output

--- kernel/x86_64/zdot_microk_steamroller-2.c
+++ kernel/x86_64/zdot_microk_steamroller-2.c
@@ -97,8 +97,8 @@ static void zdot_kernel_8( BLASLONG n, FLOAT *x, FLOAT *y, FLOAT *dot)
 
 	:
         : 
-          "r" (i),	// 0	
-	  "r" (n),  	// 1
+          "+r" (i),	// 0	
+	  "+r" (n),  	// 1
           "r" (x),      // 2
           "r" (y),      // 3
           "r" (dot)     // 4
@@ -174,8 +174,8 @@ static void zdot_kernel_8( BLASLONG n, FLOAT *x, FLOAT *y, FLOAT *dot)
 
 	:
         : 
-          "r" (i),	// 0	
-	  "r" (n),  	// 1
+          "+r" (i),	// 0	
+	  "+r" (n),  	// 1
           "r" (x),      // 2
           "r" (y),      // 3
           "r" (dot)     // 4
