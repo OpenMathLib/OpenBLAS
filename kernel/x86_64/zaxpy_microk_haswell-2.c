0cfb647a577058cebeaabadbe6ef62eebd2ce49e
Martin Kroeker <martin@ruby.chemie.uni-freiburg.de>
2019-01-16 20:51:34 +0100

    Tag operands 0 and 1 as both input and output

--- kernel/x86_64/zaxpy_microk_haswell-2.c
+++ kernel/x86_64/zaxpy_microk_haswell-2.c
@@ -113,8 +113,8 @@ static void zaxpy_kernel_4( BLASLONG n, FLOAT *x, FLOAT *y, FLOAT *alpha)
 
 	:
         : 
-          "r" (i),	// 0	
-	  "r" (n),  	// 1
+          "+r" (i),	// 0	
+	  "+r" (n),  	// 1
           "r" (x),      // 2
           "r" (y),      // 3
           "r" (alpha),  // 4
