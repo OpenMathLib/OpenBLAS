9d46f84f24dc7284fc398574b811621e5c61e2dc
Martin Kroeker <martin@ruby.chemie.uni-freiburg.de>
2019-01-16 20:27:48 +0100

    Tag operands 0 and 1 as both input and output

--- kernel/x86_64/daxpy_microk_sandy-2.c
+++ kernel/x86_64/daxpy_microk_sandy-2.c
@@ -101,8 +101,8 @@ static void daxpy_kernel_8( BLASLONG n, FLOAT *x, FLOAT *y, FLOAT *alpha)
 
 	:
         : 
-          "r" (i),	// 0	
-	  "r" (n),  	// 1
+          "+r" (i),	// 0	
+	  "+r" (n),  	// 1
           "r" (x),      // 2
           "r" (y),      // 3
           "r" (alpha)   // 4
