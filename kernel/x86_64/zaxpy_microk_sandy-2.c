2b542d10368cbb8433b7274fb12b77845606d2fe
Martin Kroeker <martin@ruby.chemie.uni-freiburg.de>
2019-01-16 20:52:35 +0100

    Tag operands 0 and 1 as both input and output

--- kernel/x86_64/zaxpy_microk_sandy-2.c
+++ kernel/x86_64/zaxpy_microk_sandy-2.c
@@ -101,8 +101,8 @@ static void zaxpy_kernel_4( BLASLONG n, FLOAT *x, FLOAT *y, FLOAT *alpha)
 
 	:
         : 
-          "r" (i),	// 0	
-	  "r" (n),  	// 1
+          "+r" (i),	// 0	
+	  "+r" (n),  	// 1
           "r" (x),      // 2
           "r" (y),      // 3
           "r" (alpha),  // 4
@@ -178,8 +178,8 @@ static void zaxpy_kernel_4( BLASLONG n, FLOAT *x, FLOAT *y, FLOAT *alpha)
 
 	:
         : 
-          "r" (i),	// 0	
-	  "r" (n),  	// 1
+          "+r" (i),	// 0	
+	  "+r" (n),  	// 1
           "r" (x),      // 2
           "r" (y),      // 3
           "r" (alpha),  // 4
