aa799573b5f91e786ef41116b9fd030161fb6a10
Martin Kroeker <martin@ruby.chemie.uni-freiburg.de>
2019-01-17 09:11:59 +0100

    Update caxpy_microk_steamroller-2.c

--- kernel/x86_64/caxpy_microk_steamroller-2.c
+++ kernel/x86_64/caxpy_microk_steamroller-2.c
@@ -115,7 +115,7 @@ static void caxpy_kernel_8( BLASLONG n, FLOAT *x, FLOAT *y, FLOAT *alpha)
 
 	: 
           "+r" (i),	// 0	
-	  "+r" (n),  	// 1
+	  "+r" (n)  	// 1
         :
           "r" (x),      // 2
           "r" (y),      // 3
@@ -182,7 +182,7 @@ static void caxpy_kernel_8( BLASLONG n, FLOAT *x, FLOAT *y, FLOAT *alpha)
 
 	:
           "+r" (i),	// 0	
-	  "+r" (n),  	// 1
+	  "+r" (n)  	// 1
         :
           "r" (x),      // 2
           "r" (y),      // 3
