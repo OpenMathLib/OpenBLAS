8f09f06f2c964ece75730dadd99e569844497fe6
Martin Kroeker <martin@ruby.chemie.uni-freiburg.de>
2019-01-17 09:30:43 +0100

    Update zdot_microk_haswell-2.c

--- kernel/x86_64/zdot_microk_haswell-2.c
+++ kernel/x86_64/zdot_microk_haswell-2.c
@@ -103,7 +103,7 @@ static void zdot_kernel_8( BLASLONG n, FLOAT *x, FLOAT *y, FLOAT *dot)
 
 	: 
           "+r" (i),	// 0	
-	  "+r" (n),  	// 1
+	  "+r" (n)  	// 1
         :
           "r" (x),      // 2
           "r" (y),      // 3
@@ -188,7 +188,7 @@ static void zdot_kernel_8( BLASLONG n, FLOAT *x, FLOAT *y, FLOAT *dot)
 
 	: 
           "+r" (i),	// 0	
-	  "+r" (n),  	// 1
+	  "+r" (n)  	// 1
         :
           "r" (x),      // 2
           "r" (y),      // 3
