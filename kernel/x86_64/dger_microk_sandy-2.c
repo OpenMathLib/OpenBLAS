d3e7e25bfb73e16bdbf89ee07d0ab584339be2a0
Martin Kroeker <martin@ruby.chemie.uni-freiburg.de>
2019-01-17 09:35:56 +0100

    Remove stray comma

--- kernel/x86_64/dger_microk_sandy-2.c
+++ kernel/x86_64/dger_microk_sandy-2.c
@@ -106,7 +106,7 @@ static void dger_kernel_16( BLASLONG n, FLOAT *x, FLOAT *y, FLOAT *alpha)
 
 	:
           "+r" (i),	// 0	
-	  "+r" (n),  	// 1
+	  "+r" (n)  	// 1
 	:
           "r" (x),      // 2
           "r" (y),      // 3
