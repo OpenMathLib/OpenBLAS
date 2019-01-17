ab8cc007364b9477e13c107a7befce7668c10ebb
Martin Kroeker <martin@ruby.chemie.uni-freiburg.de>
2019-01-17 09:18:47 +0100

    Update cdot_microk_steamroller-2.c

--- kernel/x86_64/cdot_microk_steamroller-2.c
+++ kernel/x86_64/cdot_microk_steamroller-2.c
@@ -98,7 +98,7 @@ static void cdot_kernel_16( BLASLONG n, FLOAT *x, FLOAT *y, FLOAT *dot)
 
 	:
           "+r" (i),	// 0	
-	  "+r" (n),  	// 1
+	  "+r" (n)  	// 1
         :
           "r" (x),      // 2
           "r" (y),      // 3
@@ -177,7 +177,7 @@ static void cdot_kernel_16( BLASLONG n, FLOAT *x, FLOAT *y, FLOAT *dot)
 
 	: 
           "+r" (i),	// 0	
-	  "+r" (n),  	// 1
+	  "+r" (n)  	// 1
         :
           "r" (x),      // 2
           "r" (y),      // 3
