b52e763084040ed624fff574fba1fe1bc58b1cc7
Martin Kroeker <martin@ruby.chemie.uni-freiburg.de>
2019-01-17 09:24:16 +0100

    Update sdot_microk_bulldozer-2.c

--- kernel/x86_64/sdot_microk_bulldozer-2.c
+++ kernel/x86_64/sdot_microk_bulldozer-2.c
@@ -68,7 +68,7 @@ static void sdot_kernel_16( BLASLONG n, FLOAT *x, FLOAT *y, FLOAT *dot)
 
 	: 
           "+r" (i),	// 0	
-	  "+r" (n),  	// 1
+	  "+r" (n)  	// 1
         :
           "r" (x),      // 2
           "r" (y),      // 3
