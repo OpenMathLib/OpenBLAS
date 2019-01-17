cb75878f98892850b29fc7a0b427500a56d244dd
Martin Kroeker <martin@ruby.chemie.uni-freiburg.de>
2019-01-17 09:23:16 +0100

    Update saxpy_microk_piledriver-2.c

--- kernel/x86_64/saxpy_microk_piledriver-2.c
+++ kernel/x86_64/saxpy_microk_piledriver-2.c
@@ -80,7 +80,7 @@ static void saxpy_kernel_16( BLASLONG n, FLOAT *x, FLOAT *y, FLOAT *alpha)
 
 	: 
           "+r" (i),	// 0	
-	  "+r" (n),  	// 1
+	  "+r" (n)  	// 1
         :
           "r" (x),      // 2
           "r" (y),      // 3
@@ -141,7 +141,7 @@ static void saxpy_kernel_16( BLASLONG n, FLOAT *x, FLOAT *y, FLOAT *alpha)
 
 	: 
           "+r" (i),	// 0	
-	  "+r" (n),  	// 1
+	  "+r" (n)  	// 1
         :
           "r" (x),      // 2
           "r" (y),      // 3
