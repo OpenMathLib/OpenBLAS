0779654cb47dbc9984f344d5b7ffa68e39afdbc3
Martin Kroeker <martin@ruby.chemie.uni-freiburg.de>
2019-01-17 09:16:26 +0100

    Update ddot_microk_haswell-2.c

--- kernel/x86_64/ddot_microk_haswell-2.c
+++ kernel/x86_64/ddot_microk_haswell-2.c
@@ -78,7 +78,7 @@ static void ddot_kernel_8( BLASLONG n, FLOAT *x, FLOAT *y, FLOAT *dot)
 
 	:
           "+r" (i),	// 0	
-	  "+r" (n),  	// 1
+	  "+r" (n)  	// 1
         :
           "r" (x),      // 2
           "r" (y),      // 3
