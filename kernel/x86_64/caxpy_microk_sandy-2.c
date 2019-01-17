6f7f9967f945c145e6e4ceac14162e8dbc551f4c
Martin Kroeker <martin@ruby.chemie.uni-freiburg.de>
2019-01-17 09:11:21 +0100

    Update caxpy_microk_sandy-2.c

--- kernel/x86_64/caxpy_microk_sandy-2.c
+++ kernel/x86_64/caxpy_microk_sandy-2.c
@@ -97,7 +97,7 @@ static void caxpy_kernel_8( BLASLONG n, FLOAT *x, FLOAT *y, FLOAT *alpha)
 
 	: 
           "+r" (i),	// 0	
-	  "+r" (n),  	// 1
+	  "+r" (n)  	// 1
         :
           "r" (x),      // 2
           "r" (y),      // 3
