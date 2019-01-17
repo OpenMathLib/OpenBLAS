752d4e88089ce1ff5ab27b25de382750b5e4a9c7
Martin Kroeker <martin@ruby.chemie.uni-freiburg.de>
2019-01-17 09:28:00 +0100

    Update zaxpy_microk_haswell-2.c

--- kernel/x86_64/zaxpy_microk_haswell-2.c
+++ kernel/x86_64/zaxpy_microk_haswell-2.c
@@ -113,7 +113,7 @@ static void zaxpy_kernel_4( BLASLONG n, FLOAT *x, FLOAT *y, FLOAT *alpha)
 
 	: 
           "+r" (i),	// 0	
-	  "+r" (n),  	// 1
+	  "+r" (n)  	// 1
         :
           "r" (x),      // 2
           "r" (y),      // 3
