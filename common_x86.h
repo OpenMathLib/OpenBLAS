a970a583e130cab87a0783cacf3280c0429758e7
Martin Kroeker <martin@ruby.chemie.uni-freiburg.de>
2019-02-14 11:41:48 +0100

    Update common_x86.h

--- common_x86.h
+++ common_x86.h
@@ -187,7 +187,7 @@ static __inline int blas_quickdivide(unsigned int x, unsigned int y){
 	
   y = blas_quick_divide_table[y];
 
-  __asm__ __volatile__  ("mull %0" :"=d" (result) :"+a"(x), "0" (y));
+  __asm__ __volatile__  ("mull %0" :"=d" (result), "+a"(x): "0" (y));
 
   return result;
 #endif
