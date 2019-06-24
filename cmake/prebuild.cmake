3124b7b3368bb4228cb79a99fa16824d8d93b29a
Martin Kroeker <martin@ruby.chemie.uni-freiburg.de>
2019-06-24 19:42:43 +0200

    Try to fix build breakage on ARM32/clang

--- cmake/prebuild.cmake
+++ cmake/prebuild.cmake
@@ -59,7 +59,7 @@ set(FU "")
 if (APPLE OR (MSVC AND NOT ${CMAKE_C_COMPILER_ID} MATCHES "Clang"))
   set(FU "_")
 endif()
-if (WIN32 AND MINGW)
+if (WIN32 AND MINGW AND NOT ${CMAKE_C_COMPILER_ID} MATCHES "Clang"))
   set(FU "_")
 endif()
 
