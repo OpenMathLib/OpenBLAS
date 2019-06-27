e753474ed544aebb4cc97a80f0eb352f01de0581
Martin Kroeker <martin@ruby.chemie.uni-freiburg.de>
2019-06-27 19:01:29 +0200

    Update system.cmake

--- cmake/system.cmake
+++ cmake/system.cmake
@@ -155,7 +155,7 @@ else()
 endif ()
 
 include("${PROJECT_SOURCE_DIR}/cmake/prebuild.cmake")
-if(defined BINARY)
+if (DEFINEDt BINARY)
   message(STATUS "Compiling a ${BINARY}-bit binary.")
 endif ()
 if (NOT DEFINED NEED_PIC)
