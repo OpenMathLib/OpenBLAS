2ea2bd99c7ba038d5366e8309359d32f0d5f6cd7
Martin Kroeker <martin@ruby.chemie.uni-freiburg.de>
2020-01-03 11:10:00 +0100

    Apply LAPACKE fix for eigenvector transposition in symmetric eigensolvers
    from Reference-LAPACK PR 330

--- lapack-netlib/LAPACKE/src/lapacke_cheev_work.c
+++ lapack-netlib/LAPACKE/src/lapacke_cheev_work.c
@@ -78,7 +78,11 @@ lapack_int LAPACKE_cheev_work( int matrix_layout, char jobz, char uplo,
             info = info - 1;
         }
         /* Transpose output matrices */
-        LAPACKE_che_trans( LAPACK_COL_MAJOR, uplo, n, a_t, lda_t, a, lda );
+        if ( jobz == 'V') {
+            LAPACKE_cge_trans( LAPACK_COL_MAJOR, n, n, a_t, lda_t, a, lda );
+        } else {
+            LAPACKE_che_trans( LAPACK_COL_MAJOR, uplo, n, a_t, lda_t, a, lda );
+        }
         /* Release memory and exit */
         LAPACKE_free( a_t );
 exit_level_0:
