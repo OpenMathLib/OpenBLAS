20f2f6fc8420138f0785d338125c56e04bf86ba6
Martin Kroeker <martin@ruby.chemie.uni-freiburg.de>
2020-05-01 21:12:11 +0200

    revert last change, blas_quickdivide returns a signed int again

--- common_x86_64.h
+++ common_x86_64.h
@@ -1,4 +1,4 @@
-/*********************************************************************/
+ /*********************************************************************/
 /* Copyright 2009, 2010 The University of Texas at Austin.           */
 /* All rights reserved.                                              */
 /*                                                                   */
@@ -199,7 +199,7 @@ static __inline BLASLONG blas_quickdivide(BLASLONG x, BLASLONG y){
 #else
 extern unsigned int blas_quick_divide_table[];
 
-static __inline unsigned int blas_quickdivide(unsigned int x, unsigned int y){
+static __inline int blas_quickdivide(unsigned int x, unsigned int y){
 
   unsigned int result;
 
