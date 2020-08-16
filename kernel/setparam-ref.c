2586b26e29d3bf7985cb8f45310093d8306c9108
Martin Kroeker <martin@ruby.chemie.uni-freiburg.de>
2020-08-16 09:16:52 +0200

    Add direct_sgemm

--- kernel/setparam-ref.c
+++ kernel/setparam-ref.c
@@ -135,6 +135,10 @@ gotoblas_t TABLE_NAME = {
   sgemv_nTS,  sgemv_tTS, sger_kTS,
   ssymv_LTS, ssymv_UTS,
 
+  sgemm_directTS,
+  sgemm_direct_performantTS,
+
+
   sgemm_kernelTS, sgemm_betaTS,
 #if SGEMM_DEFAULT_UNROLL_M != SGEMM_DEFAULT_UNROLL_N
   sgemm_incopyTS, sgemm_itcopyTS,
