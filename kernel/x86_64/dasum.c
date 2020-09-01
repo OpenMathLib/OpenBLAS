1b0f17eeed840d8e9642afd7d801259279d587cf
Gengxin Xie <gengxin.xie@intel.com>
2020-09-01 15:41:48 +0800

    align to 64, using SSE when input size is small

--- kernel/x86_64/dasum.c
+++ kernel/x86_64/dasum.c
@@ -1,7 +1,8 @@
 #include "common.h"
-#include <math.h>
 
-#define ABS fabs
+#ifndef ABS_K
+#define ABS_K(a) ((a) > 0 ? (a) : (-(a)))
+#endif
 
 #if defined(SKYLAKEX)
 #include "dasum_microk_skylakex-2.c"
@@ -9,88 +10,73 @@
 #include "dasum_microk_haswell-2.c"
 #endif
 
-#ifndef HAVE_KERNEL_16
-static FLOAT dasum_kernel_16(BLASLONG n, FLOAT *x1)
+#ifndef HAVE_DASUM_KERNEL
+static FLOAT dasum_kernel(BLASLONG n, FLOAT *x1)
 {
 
-	BLASLONG i=0;
-	FLOAT *x = x1;
-	FLOAT temp0, temp1, temp2, temp3;
-	FLOAT temp4, temp5, temp6, temp7;
-	FLOAT sum0 = 0.0;
-	FLOAT sum1 = 0.0;
-	FLOAT sum2 = 0.0;
-	FLOAT sum3 = 0.0;
+    BLASLONG i=0;
+    BLASLONG n_8 = n & -8;
+    FLOAT *x = x1;
+    FLOAT temp0, temp1, temp2, temp3;
+    FLOAT temp4, temp5, temp6, temp7;
+    FLOAT sum0 = 0.0;
+    FLOAT sum1 = 0.0;
+    FLOAT sum2 = 0.0;
+    FLOAT sum3 = 0.0;
+    FLOAT sum4 = 0.0;
+    
+    while (i < n_8) {
+        temp0 = ABS_K(x[0]);
+        temp1 = ABS_K(x[1]);
+        temp2 = ABS_K(x[2]);
+        temp3 = ABS_K(x[3]);
+        temp4 = ABS_K(x[4]);
+        temp5 = ABS_K(x[5]);
+        temp6 = ABS_K(x[6]);
+        temp7 = ABS_K(x[7]);
+        
+        sum0 += temp0;
+        sum1 += temp1;
+        sum2 += temp2;
+        sum3 += temp3;
+        
+        sum0 += temp4;
+        sum1 += temp5;
+        sum2 += temp6;
+        sum3 += temp7;
+        
+        x+=8;
+        i+=8;
+    }
 
-	while ( i< n )
-	{
+     while (i < n) {
+        sum4 += ABS_K(x1[i]);
+        i++;
+     }
 
-		temp0 = ABS(x[0]);
-		temp1 = ABS(x[1]);
-		temp2 = ABS(x[2]);
-		temp3 = ABS(x[3]);
-		temp4 = ABS(x[4]);
-		temp5 = ABS(x[5]);
-		temp6 = ABS(x[6]);
-		temp7 = ABS(x[7]);
-
-		sum0 += temp0;
-		sum1 += temp1;
-		sum2 += temp2;
-		sum3 += temp3;
-
-		sum0 += temp4;
-		sum1 += temp5;
-		sum2 += temp6;
-		sum3 += temp7;
-
-		x+=8;
-		i+=8;
-
-	}
-
-	return sum0+sum1+sum2+sum3;
+    return sum0+sum1+sum2+sum3+sum4;
 }
 
 #endif
 
 FLOAT CNAME(BLASLONG n, FLOAT *x, BLASLONG inc_x)
 {
-	BLASLONG i=0;
-	FLOAT sumf = 0.0;
-	BLASLONG n1;
+    BLASLONG i=0;
+    FLOAT sumf = 0.0;
 
-	if (n <= 0 || inc_x <= 0) return(sumf);
+    if (n <= 0 || inc_x <= 0) return(sumf);
 
-	if ( inc_x == 1 )
-	{
-
-		n1 = n & -16;
-		if ( n1 > 0 )
-		{
-
-			sumf = dasum_kernel_16(n1, x);
-			i=n1;
-		}
-
-		while(i < n)
-		{
-			sumf += ABS(x[i]);
-			i++;
-		}
-
-	}
-	else
-	{
-
-		n *= inc_x;
-		while(i < n)
-		{
-			sumf += ABS(x[i]);
-			i += inc_x;
-		}
-
-	}
-	return(sumf);
+    if ( inc_x == 1 ) {
+        sumf = dasum_kernel(n, x);
+    } 
+    else {
+        n *= inc_x;
+       
+        while(i < n) {
+            sumf += ABS_K(x[i]);
+            i += inc_x;
+        }
+    }
+    return(sumf);
 }
 
--- kernel/x86_64/sasum.c
+++ kernel/x86_64/sasum.c
@@ -1,13 +1,11 @@
 #include "common.h"
-#include <math.h>
 
 #if defined(DOUBLE)
-
 #error supports float only
-
 #else
-
-#define ABS fabsf
+#ifndef ABS_K
+#define ABS_K(a) ((a) > 0 ? (a) : (-(a)))
+#endif
 
 #endif
 
@@ -17,88 +15,76 @@
 #include "sasum_microk_haswell-2.c"
 #endif
 
-#ifndef HAVE_KERNEL_32
+#ifndef HAVE_SASUM_KERNEL
 
-static FLOAT sasum_kernel_32(BLASLONG n, FLOAT *x1)
+static FLOAT sasum_kernel(BLASLONG n, FLOAT *x1)
 {
 
-	BLASLONG i=0;
-	FLOAT *x = x1;
-	FLOAT temp0, temp1, temp2, temp3;
-	FLOAT temp4, temp5, temp6, temp7;
-	FLOAT sum0 = 0.0;
-	FLOAT sum1 = 0.0;
-	FLOAT sum2 = 0.0;
-	FLOAT sum3 = 0.0;
+    BLASLONG i=0;
+    BLASLONG n_8 = n & -8;
+    FLOAT *x = x1;
+    FLOAT temp0, temp1, temp2, temp3;
+    FLOAT temp4, temp5, temp6, temp7;
+    FLOAT sum0 = 0.0;
+    FLOAT sum1 = 0.0;
+    FLOAT sum2 = 0.0;
+    FLOAT sum3 = 0.0;
+    FLOAT sum4 = 0.0;
 
-	while ( i< n )
-	{
+    while (i < n_8) {
 
-		temp0 = ABS(x[0]);
-		temp1 = ABS(x[1]);
-		temp2 = ABS(x[2]);
-		temp3 = ABS(x[3]);
-		temp4 = ABS(x[4]);
-		temp5 = ABS(x[5]);
-		temp6 = ABS(x[6]);
-		temp7 = ABS(x[7]);
+        temp0 = ABS_K(x[0]);
+        temp1 = ABS_K(x[1]);
+        temp2 = ABS_K(x[2]);
+        temp3 = ABS_K(x[3]);
+        temp4 = ABS_K(x[4]);
+        temp5 = ABS_K(x[5]);
+        temp6 = ABS_K(x[6]);
+        temp7 = ABS_K(x[7]);
 
-		sum0 += temp0;
-		sum1 += temp1;
-		sum2 += temp2;
-		sum3 += temp3;
+        sum0 += temp0;
+        sum1 += temp1;
+        sum2 += temp2;
+        sum3 += temp3;
 
-		sum0 += temp4;
-		sum1 += temp5;
-		sum2 += temp6;
-		sum3 += temp7;
+        sum0 += temp4;
+        sum1 += temp5;
+        sum2 += temp6;
+        sum3 += temp7;
 
-		x+=8;
-		i+=8;
+        x+=8;
+        i+=8;
 
-	}
+    }
 
-	return sum0+sum1+sum2+sum3;
+    while (i < n) {
+        sum4 += ABS_K(x1[i]);
+        i++;
+    }
+
+    return sum0+sum1+sum2+sum3+sum4;
 }
 
 #endif
 
 FLOAT CNAME(BLASLONG n, FLOAT *x, BLASLONG inc_x)
 {
-	BLASLONG i=0;
-	FLOAT sumf = 0.0;
-	BLASLONG n1;
+    BLASLONG i=0;
+    FLOAT sumf = 0.0;
 
-	if (n <= 0 || inc_x <= 0) return(sumf);
+    if (n <= 0 || inc_x <= 0) return(sumf);
 
-	if ( inc_x == 1 )
-	{
+    if ( inc_x == 1 ) {
+        sumf = sasum_kernel(n, x);
+    }
+    else {
 
-		n1 = n & -32;
-		if ( n1 > 0 )
-		{
+        n *= inc_x;
+        while(i < n) {
+            sumf += ABS_K(x[i]);
+            i += inc_x;
+        }
 
-			sumf = sasum_kernel_32(n1, x);
-			i=n1;
-		}
-
-		while(i < n)
-		{
-			sumf += ABS(x[i]);
-			i++;
-		}
-
-	}
-	else
-	{
-
-		n *= inc_x;
-		while(i < n)
-		{
-			sumf += ABS(x[i]);
-			i += inc_x;
-		}
-
-	}
-	return(sumf);
+    }
+    return(sumf);
 }
