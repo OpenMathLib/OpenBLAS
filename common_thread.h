b0a7c63e6a4d54ea9099931a8ed945a7ce452491
Martin Kroeker <martin@ruby.chemie.uni-freiburg.de>
2021-04-12 18:15:54 +0200

    try reverting PR2706

--- common_thread.h
+++ common_thread.h
@@ -139,18 +139,19 @@ extern int blas_server_avail;
 static __inline int num_cpu_avail(int level) {
 
 #ifdef USE_OPENMP
-	int openmp_nthreads=omp_get_max_threads();
+	int openmp_nthreads=0; //omp_get_max_threads();
 #endif
 
-#ifndef USE_OPENMP 
+//#ifndef USE_OPENMP 
   if (blas_cpu_number == 1
-#endif
+//#endif
 #ifdef USE_OPENMP
-     if (openmp_nthreads == 1 || omp_in_parallel()
+     || omp_in_parallel()
 #endif
       ) return 1;        
 
 #ifdef USE_OPENMP
+	openmp_nthreads = omp_get_max_threads();
   if (blas_cpu_number != openmp_nthreads) {
 	  goto_set_num_threads(openmp_nthreads);
   }
