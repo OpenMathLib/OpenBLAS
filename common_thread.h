7f06960a8858a22215a4739facd098b6495090bb
Martin Kroeker <martin@ruby.chemie.uni-freiburg.de>
2021-04-13 12:01:06 +0200

    Update common_thread.h

--- common_thread.h
+++ common_thread.h
@@ -138,6 +138,8 @@ extern int blas_server_avail;
 
 static __inline int num_cpu_avail(int level) {
 
+	return 1;
+	
 #ifdef USE_OPENMP
 	int openmp_nthreads=0; //omp_get_max_threads();
 #endif
@@ -151,7 +153,7 @@ static __inline int num_cpu_avail(int level) {
       ) return 1;        
 
 #ifdef USE_OPENMP
-	openmp_nthreads = 2; //omp_get_max_threads();
+	openmp_nthreads = omp_get_max_threads();
   if (blas_cpu_number != openmp_nthreads) {
 	  goto_set_num_threads(openmp_nthreads);
   }
