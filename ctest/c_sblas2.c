c88b260a436eda3b921a0ad82284334d62fe8718
Martin Kroeker <martin@ruby.chemie.uni-freiburg.de>
2021-04-15 14:32:14 +0200

    Update c_sblas2.c

--- ctest/c_sblas2.c
+++ ctest/c_sblas2.c
@@ -30,7 +30,7 @@ void F77_sgemv(blasint *order, char *transp, blasint *m, blasint *n, float *alph
      free(A);
   }
   else if (*order == TEST_COL_MJR) {
-	  fprintf(stderr,"calling cblas_sgemv COL %s %d %ld %ld from c_cblas2.c\n", transp,trans,*m,*n);
+	  fprintf(stderr,"calling cblas_sgemv COL %s %d %ld %ld %f a %ld x %ld %f y %ld from c_cblas2.c\n", transp,trans,*m,*n,alpha,lda,incx,beta,incy);
      cblas_sgemv( CblasColMajor, trans,
 		  *m, *n, *alpha, a, *lda, x, *incx, *beta, y, *incy );
 	  fprintf(stderr,"calling cblas_sgemv COL from c_cblas2.c done\n");
