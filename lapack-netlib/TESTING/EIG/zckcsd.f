cd0e4aadb1ee504371ba6fd516dcd5a3d9b65e95
Martin Kroeker <martin@ruby.chemie.uni-freiburg.de>
2021-06-16 09:41:18 +0200

    Declare ZDROT as EXTERNAL

--- lapack-netlib/TESTING/EIG/zckcsd.f
+++ lapack-netlib/TESTING/EIG/zckcsd.f
@@ -228,7 +228,7 @@
 *     ..
 *     .. External Subroutines ..
       EXTERNAL           ALAHDG, ALAREQ, ALASUM, ZCSDTS, ZLACSG, ZLAROR,
-     $                   ZLASET
+     $                   ZLASET, ZDROT
 *     ..
 *     .. Intrinsic Functions ..
       INTRINSIC          ABS, MIN
