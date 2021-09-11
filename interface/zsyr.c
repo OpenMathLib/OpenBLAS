20581bf303776f831c788ced24f179d720ec5c39
Martin Kroeker <martin@ruby.chemie.uni-freiburg.de>
2021-09-11 14:36:27 +0200

    Remove unused variable

--- interface/zsyr.c
+++ interface/zsyr.c
@@ -119,7 +119,7 @@ void NAME(char *UPLO, blasint *N, FLOAT  *ALPHA,
 void CNAME(enum CBLAS_ORDER order, enum CBLAS_UPLO Uplo, int n, FLOAT alpha, FLOAT *x, int incx, FLOAT *a, int lda) {
 
   FLOAT *buffer;
-  int trans, uplo;
+  int uplo;
   blasint info;
   FLOAT * ALPHA = &alpha;
   FLOAT alpha_r	= ALPHA[0];
