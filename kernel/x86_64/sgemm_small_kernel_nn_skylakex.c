abbc947edb830af96fc72ce7789f954737805830
Mosè Giordano <mose@gnu.org>
2022-02-23 22:51:59 +0000

    Fix compilation of Skylake AVX512 kernels with GCC 6

--- kernel/x86_64/sgemm_small_kernel_nn_skylakex.c
+++ kernel/x86_64/sgemm_small_kernel_nn_skylakex.c
@@ -612,6 +612,6 @@ int CNAME(BLASLONG M, BLASLONG N, BLASLONG K, FLOAT * A, BLASLONG lda, FLOAT alp
 	return 0;
 }
 #else
-#include ../generic/gemm_small_matrix_kernel_nn.c
+#include "../generic/gemm_small_matrix_kernel_nn.c"
 #endif
 
