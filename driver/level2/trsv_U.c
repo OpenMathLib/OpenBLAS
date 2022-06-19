e10802018d9f862dd0cd55cab860fa7de9efa482
Martin Kroeker <martin@ruby.chemie.uni-freiburg.de>
2022-06-19 17:16:47 +0200

    Update trsv_U.c

--- driver/level2/trsv_U.c
+++ driver/level2/trsv_U.c
@@ -42,7 +42,11 @@
 
 const static FLOAT dm1 = -1.;
 
-int __attribute__((visibility("hidden"))) CNAME(BLASLONG m, FLOAT *a, BLASLONG lda, FLOAT *b, BLASLONG incb, void *buffer){
+int
+#ifndef MSVC
+__attribute__((visibility("hidden"))) 
+#endif
+	CNAME(BLASLONG m, FLOAT *a, BLASLONG lda, FLOAT *b, BLASLONG incb, void *buffer){
 
   BLASLONG i, is, min_i;
   FLOAT *gemvbuffer = (FLOAT *)buffer;
