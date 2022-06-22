3371bd85a30f529925026fca394c3f02962f5a1c
Martin Kroeker <martin@ruby.chemie.uni-freiburg.de>
2022-06-22 14:46:32 +0200

    Set visibility of internal symbols to hidden

--- driver/level2/ztpsv_U.c
+++ driver/level2/ztpsv_U.c
@@ -40,7 +40,11 @@
 #include <ctype.h>
 #include "common.h"
 
-int CNAME(BLASLONG m, FLOAT *a, FLOAT *b, BLASLONG incb, void *buffer){
+int
+#ifndef C_MSVC
+__attribute__((visibility("hidden")))
+#endif
+	CNAME(BLASLONG m, FLOAT *a, FLOAT *b, BLASLONG incb, void *buffer){
 
   BLASLONG i;
 #if (TRANSA == 2) || (TRANSA == 4)
