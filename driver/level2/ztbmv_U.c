b137520315485b1b514b059bce246b01c325788d
Martin Kroeker <martin@ruby.chemie.uni-freiburg.de>
2022-06-22 18:36:10 +0200

    Set visibility of internal symbols to hidden

--- driver/level2/ztbmv_U.c
+++ driver/level2/ztbmv_U.c
@@ -42,7 +42,11 @@
 
 // const static FLOAT dp1 = 1.;
 
-int CNAME(BLASLONG n, BLASLONG k, FLOAT *a, BLASLONG lda, FLOAT *b, BLASLONG incb, void *buffer){
+int
+#ifndef C_MSVC
+__attribute__((visibility("hidden")))
+#endif
+	CNAME(BLASLONG n, BLASLONG k, FLOAT *a, BLASLONG lda, FLOAT *b, BLASLONG incb, void *buffer){
 
   BLASLONG i;
   FLOAT *B = b;
