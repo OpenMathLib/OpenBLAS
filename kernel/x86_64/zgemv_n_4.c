e5d0749420939386a039add869a02b210e19b9f6
Martin Kroeker <martin@ruby.chemie.uni-freiburg.de>
2022-09-01 11:16:16 +0200

    try pragma to disable tree vectorizer

--- kernel/x86_64/zgemv_n_4.c
+++ kernel/x86_64/zgemv_n_4.c
@@ -24,7 +24,7 @@ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
 OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
 USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 *****************************************************************************/
-
+#pragma GCC optimize("no-tree-vectorize")
 #include <stdlib.h>
 #include <stdio.h>
 #include "common.h"
