90b2d9fe3e50007e8e28f5400126bd1ce9ed47b0
Martin Kroeker <martin@ruby.chemie.uni-freiburg.de>
2022-09-03 12:24:56 +0200

    move pragma

--- kernel/x86_64/ssymv_U.c
+++ kernel/x86_64/ssymv_U.c
@@ -24,11 +24,12 @@ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
 OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
 USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 *****************************************************************************/
+
+#include "common.h"
+
 #if (defined(OS_DARWIN) || defined(OS_WINDOWS)) && (defined(__GNUC__) && __GNUC__ > 11) 
 #pragma GCC optimize("no-tree-vectorize")
 #endif
-
-#include "common.h"
 
 
 #if defined(BULLDOZER) || defined(PILEDRIVER) || defined(STEAMROLLER)  || defined(EXCAVATOR)
