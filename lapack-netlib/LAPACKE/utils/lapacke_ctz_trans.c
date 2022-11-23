7694ff495f26c29cc24771a29ccd13f4191a2baf
Martin Kroeker <martin@ruby.chemie.uni-freiburg.de>
2022-11-23 10:40:59 +0100

    Remove unnecessary return in void function call (Reference-LAPACK PR760)

--- lapack-netlib/LAPACKE/utils/lapacke_ctz_trans.c
+++ lapack-netlib/LAPACKE/utils/lapacke_ctz_trans.c
@@ -147,7 +147,7 @@ void LAPACKE_ctz_trans( int matrix_layout, char direct, char uplo,
     }
 
     /* Copy & transpose triangular part */
-    return LAPACKE_ctr_trans( matrix_layout, uplo, diag, tri_n,
-                              &in[tri_in_offset], ldin,
-                              &out[tri_out_offset], ldout );
+        LAPACKE_ctr_trans( matrix_layout, uplo, diag, tri_n,
+                            &in[tri_in_offset], ldin,
+                            &out[tri_out_offset], ldout );
 }
