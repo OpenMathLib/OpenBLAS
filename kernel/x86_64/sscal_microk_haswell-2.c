60e49b851c501e0311916d64a5d122faaa983f45
Bart Oldeman <bartoldeman@users.noreply.github.com>
2022-12-06 16:30:46 -0500

    Fix typo in clobber list, should be xmm14 instead of ymm14.

--- kernel/x86_64/sscal_microk_haswell-2.c
+++ kernel/x86_64/sscal_microk_haswell-2.c
@@ -111,7 +111,7 @@ static void sscal_kernel_16( BLASLONG n, FLOAT *alpha, FLOAT *x)
 	  "%xmm0", "%xmm1", "%xmm2", "%xmm3",
 	  "%xmm4", "%xmm5", "%xmm6", "%xmm7",
 	  "%xmm8", "%xmm9", "%xmm10", "%xmm11",
-	  "%xmm12", "%xmm13", "%ymm14", "%xmm15",
+	  "%xmm12", "%xmm13", "%xmm14", "%xmm15",
 	  "memory"
 	);
 
