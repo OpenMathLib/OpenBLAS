84bcf6639fb2de9ae15350a6b3979bf5757bae85
Martin Kroeker <martin@ruby.chemie.uni-freiburg.de>
2023-04-20 23:24:52 +0200

    Disable gcc's tree-vectorizer pass on all operating systems

--- kernel/x86_64/cgemv_t_4.c
+++ kernel/x86_64/cgemv_t_4.c
@@ -24,7 +24,9 @@ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
 OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
 USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 *****************************************************************************/
-
+#if (defined(__GNUC__) && __GNUC__ > 11) 
+#pragma GCC optimize("no-tree-vectorize")
+#endif
 
 #include "common.h"
 
