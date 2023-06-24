60014180570528e0d6ae147dac66be94e5241170
Martin Kroeker <martin@ruby.chemie.uni-freiburg.de>
2023-06-24 12:34:43 +0200

    Add pragma to restrict optimization

--- kernel/riscv64/copy_vector.c
+++ kernel/riscv64/copy_vector.c
@@ -24,6 +24,7 @@ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
 OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
 USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 *****************************************************************************/
+#pragma GCC optimize "O1"
 #include "common.h"
 #if !defined(DOUBLE)
 #define VSETVL(n) vsetvl_e32m8(n)
