0a637cc403b2af09b20c48238e2376793a25c8c2
Martin Kroeker <martin@ruby.chemie.uni-freiburg.de>
2023-07-15 16:37:42 +0200

    Fix workspace query corner cases to always return at least 1 (Reference-LAPACK PR 883) (#4146)
    * Fix workspace query corner cases to always return at least 1

--- lapack-netlib/SRC/dsytrf.f
+++ lapack-netlib/SRC/dsytrf.f
@@ -232,7 +232,7 @@
 *        Determine the block size
 *
          NB = ILAENV( 1, 'DSYTRF', UPLO, N, -1, -1, -1 )
-         LWKOPT = N*NB
+         LWKOPT = MAX( 1, N*NB )
          WORK( 1 ) = LWKOPT
       END IF
 *
