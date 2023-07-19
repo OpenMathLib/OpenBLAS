b34f19a365a43ce8a05c29a97036bfc9e300c289
Martin Kroeker <martin@ruby.chemie.uni-freiburg.de>
2023-07-19 22:19:22 +0200

    Ensure that a premature call to set_num_threads will not overwrite unrelated memory

--- driver/others/blas_server.c
+++ driver/others/blas_server.c
@@ -973,7 +973,7 @@ void goto_set_num_threads(int num_threads) {
 
     increased_threads = 1;
 
-    for(i = blas_num_threads - 1; i < num_threads - 1; i++){
+    for(i = (blas_num_threads > 0) ? blas_num_threads - 1 : 0; i < num_threads - 1; i++){
 
       atomic_store_queue(&thread_status[i].queue, (blas_queue_t *)0);
       thread_status[i].status = THREAD_STATUS_WAKEUP;
