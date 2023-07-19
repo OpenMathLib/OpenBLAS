5c58994eb26bfd5715f4100a213a0e69b280ea16
Martin Kroeker <martin@ruby.chemie.uni-freiburg.de>
2023-07-19 18:27:41 +0200

    Add fallback warning

--- driver/others/dynamic_arm64.c
+++ driver/others/dynamic_arm64.c
@@ -137,6 +137,8 @@ extern gotoblas_t  gotoblas_CORTEXA55;
 #endif
 
 extern void openblas_warning(int verbose, const char * msg);
+#define FALLBACK_VERBOSE 1
+#define NEOVERSEN1_FALLBACK "OpenBLAS : Your OS does not support SVE instructions. OpenBLAS is using Neoverse N1 kernels as a fallback, which may give poorer performance.\n"
 
 #define NUM_CORETYPES   13
 
@@ -284,14 +286,16 @@ static gotoblas_t *get_coretype(void) {
           return &gotoblas_NEOVERSEN1;
 #ifndef NO_SVE
         case 0xd49:
-	  if (!(getauxval(AT_HWCAP) & HWCAP_SVE)) 
+	  if (!(getauxval(AT_HWCAP) & HWCAP_SVE)) {
+	    openblas_warning(FALLBACK_VERBOSE, NEOVERSEN1_FALLBACK);  
 	    return &gotoblas_NEOVERSEN1;
-	  else
+	  } else
             return &gotoblas_NEOVERSEN2;
 	case 0xd40:
-	  if (!(getauxval(AT_HWCAP) & HWCAP_SVE)) 
+	  if (!(getauxval(AT_HWCAP) & HWCAP_SVE)) {
+		  openblas_warning(FALLBACK_VERBOSE, NEOVERSEN1_FALLBACK);
 	    return &gotoblas_NEOVERSEN1;
-	  else
+      	  }else
 	    return &gotoblas_NEOVERSEV1;
 #endif
 	case 0xd05: // Cortex A55
