9f6847583a5e0c5360760bd247c34bf7ce8db022
Martin Kroeker <martin@ruby.chemie.uni-freiburg.de>
2023-07-29 11:50:16 +0200

    nvc currently miscompiles this, hopefully fixed in release 23.09

--- kernel/x86_64/zasum_microk_skylakex-2.c
+++ kernel/x86_64/zasum_microk_skylakex-2.c
@@ -2,8 +2,9 @@
 #ifdef __NVCOMPILER
 #define NVCOMPVERS ( __NVCOMPILER_MAJOR__ * 100 + __NVCOMPILER_MINOR__ )
 #endif
-#if (( defined(__GNUC__)  && __GNUC__   > 6 && defined(__AVX512CD__)) || (defined(__clang__) && __clang_major__ >= 9)) || (defined(__NVCOMPILER) && NVCOMPVERS >= 2203 )
+#if (( defined(__GNUC__)  && __GNUC__   > 6 && defined(__AVX512CD__)) || (defined(__clang__) && __clang_major__ >= 9)) || (defined(__NVCOMPILER) && NVCOMPVERS >= 2309 )
 
+#if (!(defined(__NVCOMPILER) && NVCOMPVERS < 2309))
 
 #define HAVE_ZASUM_KERNEL 1
 
@@ -341,4 +342,5 @@ static FLOAT zasum_kernel(BLASLONG n, FLOAT *x)
 
     return sumf;
 }
+#endif
 #endif
