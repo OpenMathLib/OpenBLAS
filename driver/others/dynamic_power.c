574912f534380b9df8d713407ecfc2de6175acb9
Chip Kerchner <chip.kerchner@ibm.com>
2024-02-07 07:54:06 -0600

    Add missing CPU ID definitions for old versions of AIX.

--- driver/others/dynamic_power.c
+++ driver/others/dynamic_power.c
@@ -43,6 +43,13 @@ char *gotoblas_corename(void) {
 #define CPU_POWER9   9
 #define CPU_POWER10 10
 
+#ifndef POWER_9
+#define POWER_9         0x20000         /* 9 class CPU */
+#endif
+#ifndef POWER_10
+#define POWER_10        0x40000         /* 10 class CPU */
+#endif
+
 #ifdef _AIX
 #include <sys/systemcfg.h>
 
