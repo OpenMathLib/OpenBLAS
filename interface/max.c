c6f30fd4146258dcdfc7cf1322ad1e0b4f88e1ac
kseniyazaytseva <kseniya.zaytseva@syntacore.com>
2024-02-10 00:48:07 +0300

    check for zero inc

--- interface/max.c
+++ interface/max.c
@@ -46,6 +46,12 @@
 
 #ifdef USE_ABS
 
+#if defined(DOUBLE)
+#define ABS fabs
+#else
+#define ABS fabsf
+#endif
+
 #ifndef USE_MIN
 
 /* ABS & MAX */
@@ -92,6 +98,8 @@
 
 #else
 
+#define ABS
+
 #ifndef USE_MIN
 
 /* MAX */
@@ -130,6 +138,12 @@ FLOATRET NAME(blasint *N, FLOAT *x, blasint *INCX){
 
   if (n <= 0) return 0;
 
+#ifndef COMPLEX
+  if (incx == 0) return (ABS(*x));
+#else
+  if (incx == 0) return (ABS(*x) + ABS(*(x+1)));
+#endif
+
   IDEBUG_START;
 
   FUNCTION_PROFILE_START();
@@ -157,6 +171,12 @@ FLOAT CNAME(blasint n, FLOAT *x, blasint incx){
   PRINT_DEBUG_CNAME;
 
   if (n <= 0) return 0;
+
+#ifndef COMPLEX
+  if (incx == 0) return (ABS(*x));
+#else
+  if (incx == 0) return (ABS(*x) + ABS(*(x+1)));
+#endif
 
   IDEBUG_START;
 
