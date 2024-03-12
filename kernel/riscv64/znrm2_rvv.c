db70c7f7fbd64ad2f19bb104614e9f8f613eeb9b
Martin Kroeker <martin@ruby.chemie.uni-freiburg.de>
2024-03-12 20:42:11 +0100

    handle incx < 0

--- kernel/riscv64/znrm2_rvv.c
+++ kernel/riscv64/znrm2_rvv.c
@@ -69,7 +69,7 @@ FLOAT CNAME(BLASLONG n, FLOAT *x, BLASLONG inc_x)
 {
 	BLASLONG i=0, j=0;
 
-	if (n <= 0 || inc_x <= 0) return(0.0);
+	if (n <= 0 || inc_x == 0) return(0.0);
 
     FLOAT_V_T vr, v0, v_zero;
     unsigned int gvl = 0;
