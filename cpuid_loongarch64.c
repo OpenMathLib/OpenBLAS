9204077ec7327ff90c1ae7b78c5e4927f4870afe
Martin Kroeker <martin@ruby.chemie.uni-freiburg.de>
2024-04-02 17:18:39 +0200

    include stdio for printf

--- cpuid_loongarch64.c
+++ cpuid_loongarch64.c
@@ -30,7 +30,7 @@ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
 USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 **********************************************************************************/
-
+#include <stdio.h>
 #include <stdint.h>
 #include <sys/auxv.h>
 
