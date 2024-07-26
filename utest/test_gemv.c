a090011fbff0d15b63a7edcc0156ed790e87f4b1
Martin Kroeker <martin@ruby.chemie.uni-freiburg.de>
2024-07-26 12:56:12 +0200

    just use numeric constants in dimensions

--- utest/test_gemv.c
+++ utest/test_gemv.c
@@ -7,22 +7,21 @@
 #ifndef INFINITY
 #define INFINITY 1.0/0.0
 #endif
-#define N 17
-#define Ny 33
 
 #ifdef BUILD_SINGLE
 
 CTEST(sgemv, 0_nan_inf)
 {
     int i;
+    blasint N = 17;
     blasint incX = 1;
     blasint incY = 1;
     float alpha = 0.0;
     float beta = 0.0;
     char  trans = 'N';
-    float A[N * N];
-    float X[N];
-    float Y[N];
+    float A[17 * 17];
+    float X[17];
+    float Y[17];
 
     memset(A, 0, sizeof(A));
     memset(X, 0, sizeof(X));
@@ -40,14 +39,16 @@ CTEST(sgemv, 0_nan_inf)
 CTEST(sgemv, 0_nan_inf_incy_2)
 {
     int i;
+    blasint N = 17;
+    blasint Ny = 33;
     blasint incX = 1;
     blasint incY = 2;
     float alpha = 0.0;
     float beta = 0.0;
     char  trans = 'N';
-    float A[N * N];
-    float X[N];
-    float Y[Ny];
+    float A[17 * 17];
+    float X[17];
+    float Y[33];
     float *ay = Y;
 
     memset(A, 0, sizeof(A));
@@ -72,14 +73,15 @@ CTEST(sgemv, 0_nan_inf_incy_2)
 CTEST(dgemv, 0_nan_inf)
 {
     int i;
+    blasint N = 17;
     blasint incX = 1;
     blasint incY = 1;
     double alpha = 0.0;
     double beta = 0.0;
     char  trans = 'N';
-    double A[N * N];
-    double X[N];
-    double Y[N];
+    double A[17 * 17];
+    double X[17];
+    double Y[17];
 
     memset(A, 0, sizeof(A));
     memset(X, 0, sizeof(X));
@@ -97,14 +99,16 @@ CTEST(dgemv, 0_nan_inf)
 CTEST(dgemv, 0_nan_inf_incy_2)
 {
     int i;
+    blasint N = 17;
+    blasint Ny = 33;
     blasint incX = 1;
     blasint incY = 2;
     double alpha = 0.0;
     double beta = 0.0;
     char  trans = 'N';
-    double A[N * N];
-    double X[N];
-    double Y[Ny];
+    double A[17 * 17];
+    double X[17];
+    double Y[33];
     double *ay = Y;
 
     memset(A, 0, sizeof(A));
