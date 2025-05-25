7b915870eb9df6d2bd194bf495e947082b47b335
Martin Kroeker <martin@ruby.chemie.uni-freiburg.de>
2025-05-25 12:29:26 +0200

    Update cscal.c

--- kernel/zarch/cscal.c
+++ kernel/zarch/cscal.c
@@ -358,7 +358,7 @@ int CNAME(BLASLONG n, BLASLONG dummy0, BLASLONG dummy1, FLOAT da_r, FLOAT da_i,
       float res = 0.0;
       if (isnan(da_r)) res = da_r;
       while (j < n) {
-
+	if (!dummy2 || !isnan(x[i]))
         x[i] = res;
         x[i + 1] = res;
         i += 2;
