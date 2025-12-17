651578ebfaa5e813ffc4ff3e3039e31126325273
Anna Mayne <anna.mayne@arm.com>
2025-12-17 11:58:24 +0000

    Fixed builds and added missing copyright notices. Also fixed formatting of copyright notices added in last commit.

--- common_s.h
+++ common_s.h
@@ -1,3 +1,42 @@
+/*********************************************************************/
+/* Copyright 2009, 2010 The University of Texas at Austin.           */
+/* Copyright 2025 The OpenBLAS Project.                              */
+/* All rights reserved.                                              */
+/*                                                                   */
+/* Redistribution and use in source and binary forms, with or        */
+/* without modification, are permitted provided that the following   */
+/* conditions are met:                                               */
+/*                                                                   */
+/*   1. Redistributions of source code must retain the above         */
+/*      copyright notice, this list of conditions and the following  */
+/*      disclaimer.                                                  */
+/*                                                                   */
+/*   2. Redistributions in binary form must reproduce the above      */
+/*      copyright notice, this list of conditions and the following  */
+/*      disclaimer in the documentation and/or other materials       */
+/*      provided with the distribution.                              */
+/*                                                                   */
+/*    THIS  SOFTWARE IS PROVIDED  BY THE  UNIVERSITY OF  TEXAS AT    */
+/*    AUSTIN  ``AS IS''  AND ANY  EXPRESS OR  IMPLIED WARRANTIES,    */
+/*    INCLUDING, BUT  NOT LIMITED  TO, THE IMPLIED  WARRANTIES OF    */
+/*    MERCHANTABILITY  AND FITNESS FOR  A PARTICULAR  PURPOSE ARE    */
+/*    DISCLAIMED.  IN  NO EVENT SHALL THE UNIVERSITY  OF TEXAS AT    */
+/*    AUSTIN OR CONTRIBUTORS BE  LIABLE FOR ANY DIRECT, INDIRECT,    */
+/*    INCIDENTAL,  SPECIAL, EXEMPLARY,  OR  CONSEQUENTIAL DAMAGES    */
+/*    (INCLUDING, BUT  NOT LIMITED TO,  PROCUREMENT OF SUBSTITUTE    */
+/*    GOODS  OR  SERVICES; LOSS  OF  USE,  DATA,  OR PROFITS;  OR    */
+/*    BUSINESS INTERRUPTION) HOWEVER CAUSED  AND ON ANY THEORY OF    */
+/*    LIABILITY, WHETHER  IN CONTRACT, STRICT  LIABILITY, OR TORT    */
+/*    (INCLUDING NEGLIGENCE OR OTHERWISE)  ARISING IN ANY WAY OUT    */
+/*    OF  THE  USE OF  THIS  SOFTWARE,  EVEN  IF ADVISED  OF  THE    */
+/*    POSSIBILITY OF SUCH DAMAGE.                                    */
+/*                                                                   */
+/* The views and conclusions contained in the software and           */
+/* documentation are those of the authors and should not be          */
+/* interpreted as representing official policies, either expressed   */
+/* or implied, of The University of Texas at Austin.                 */
+/*********************************************************************/
+
 #ifndef COMMON_S_H
 #define COMMON_S_H
 
@@ -72,6 +111,9 @@
 #define	SGEMM_ITCOPY		sgemm_itcopy
 #endif
 
+#define	SCOMM_NCOPY		    scomm_ncopy
+#define	SCOMM_TCOPY	    	scomm_tcopy
+
 #define	STRMM_OUNUCOPY		strmm_ounucopy
 #define	STRMM_OUNNCOPY		strmm_ounncopy
 #define	STRMM_OUTUCOPY		strmm_outucopy
@@ -117,8 +159,6 @@
 #define	STRMM_ILNNCOPY		strmm_ilnncopy
 #define	STRMM_ILTUCOPY		strmm_iltucopy
 #define	STRMM_ILTNCOPY		strmm_iltncopy
-#define	SCOMM_NCOPY		    scomm_ncopy
-#define	SCOMM_TCOPY	    	scomm_tcopy
 
 #define	STRSM_IUNUCOPY		strsm_iunucopy
 #define	STRSM_IUNNCOPY		strsm_iunncopy
--- common_z.h
+++ common_z.h
@@ -1,3 +1,42 @@
+/*********************************************************************/
+/* Copyright 2009, 2010 The University of Texas at Austin.           */
+/* Copyright 2025 The OpenBLAS Project.                              */
+/* All rights reserved.                                              */
+/*                                                                   */
+/* Redistribution and use in source and binary forms, with or        */
+/* without modification, are permitted provided that the following   */
+/* conditions are met:                                               */
+/*                                                                   */
+/*   1. Redistributions of source code must retain the above         */
+/*      copyright notice, this list of conditions and the following  */
+/*      disclaimer.                                                  */
+/*                                                                   */
+/*   2. Redistributions in binary form must reproduce the above      */
+/*      copyright notice, this list of conditions and the following  */
+/*      disclaimer in the documentation and/or other materials       */
+/*      provided with the distribution.                              */
+/*                                                                   */
+/*    THIS  SOFTWARE IS PROVIDED  BY THE  UNIVERSITY OF  TEXAS AT    */
+/*    AUSTIN  ``AS IS''  AND ANY  EXPRESS OR  IMPLIED WARRANTIES,    */
+/*    INCLUDING, BUT  NOT LIMITED  TO, THE IMPLIED  WARRANTIES OF    */
+/*    MERCHANTABILITY  AND FITNESS FOR  A PARTICULAR  PURPOSE ARE    */
+/*    DISCLAIMED.  IN  NO EVENT SHALL THE UNIVERSITY  OF TEXAS AT    */
+/*    AUSTIN OR CONTRIBUTORS BE  LIABLE FOR ANY DIRECT, INDIRECT,    */
+/*    INCIDENTAL,  SPECIAL, EXEMPLARY,  OR  CONSEQUENTIAL DAMAGES    */
+/*    (INCLUDING, BUT  NOT LIMITED TO,  PROCUREMENT OF SUBSTITUTE    */
+/*    GOODS  OR  SERVICES; LOSS  OF  USE,  DATA,  OR PROFITS;  OR    */
+/*    BUSINESS INTERRUPTION) HOWEVER CAUSED  AND ON ANY THEORY OF    */
+/*    LIABILITY, WHETHER  IN CONTRACT, STRICT  LIABILITY, OR TORT    */
+/*    (INCLUDING NEGLIGENCE OR OTHERWISE)  ARISING IN ANY WAY OUT    */
+/*    OF  THE  USE OF  THIS  SOFTWARE,  EVEN  IF ADVISED  OF  THE    */
+/*    POSSIBILITY OF SUCH DAMAGE.                                    */
+/*                                                                   */
+/* The views and conclusions contained in the software and           */
+/* documentation are those of the authors and should not be          */
+/* interpreted as representing official policies, either expressed   */
+/* or implied, of The University of Texas at Austin.                 */
+/*********************************************************************/
+
 #ifndef COMMON_Z_H
 #define COMMON_Z_H
 
@@ -62,6 +101,9 @@
 #define	ZGEMM_ITCOPY		zgemm_itcopy
 #endif
 
+#define	ZCOMM_NCOPY	    	zcomm_ncopy
+#define	ZCOMM_TCOPY	    	zcomm_tcopy
+
 #define	ZTRMM_OUNUCOPY		ztrmm_ounucopy
 #define	ZTRMM_OUNNCOPY		ztrmm_ounncopy
 #define	ZTRMM_OUTUCOPY		ztrmm_outucopy
@@ -89,8 +131,6 @@
 #define	ZTRMM_ILNNCOPY		ztrmm_olnncopy
 #define	ZTRMM_ILTUCOPY		ztrmm_oltucopy
 #define	ZTRMM_ILTNCOPY		ztrmm_oltncopy
-#define	ZCOMM_NCOPY	    	zcomm_ncopy
-#define	ZCOMM_TCOPY	    	zcomm_tcopy
 
 #define	ZTRSM_IUNUCOPY		ztrsm_ounucopy
 #define	ZTRSM_IUNNCOPY		ztrsm_ounncopy
@@ -109,8 +149,6 @@
 #define	ZTRMM_ILNNCOPY		ztrmm_ilnncopy
 #define	ZTRMM_ILTUCOPY		ztrmm_iltucopy
 #define	ZTRMM_ILTNCOPY		ztrmm_iltncopy
-#define	ZCOMM_NCOPY		    zcomm_ncopy
-#define	ZCOMM_TCOPY	    	zcomm_tcopy
 
 #define	ZTRSM_IUNUCOPY		ztrsm_iunucopy
 #define	ZTRSM_IUNNCOPY		ztrsm_iunncopy
